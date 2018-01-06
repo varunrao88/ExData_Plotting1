a264cb0191bd3d395f588b50bdfd42309a18ad76
Varun Rao <varunrao88@hotmail.com>
2018-01-07 00:29:04 +0530

    plot1.png Added
    plot1.png Added

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
 my_data <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 ele <- subset(my_data, Date == '1/2/2007' | Date == '2/2/2007')
-png(filename = "plot1-demo.png", width = 480, height=480)
+png(filename = "plot1.png", width = 480, height=480)
 hist(as.numeric(as.character(ele$Global_active_power)), main = "Global Active Power", col = "red", xlab = "Global Active Power(kilowatts)")
 dev.off()
+
